6bbdf4bae4022ac33d8a7f0d327a75b7987cc461
whytomato <1158152324@qq.com>
2023-11-14 21:01:30 +0800

    挂载一个axios

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+const app = createApp(App)
+app.use(router).use(store)
+app.use(VueAxios, axios)
 
-createApp(App).use(store).use(router).mount('#app')
+app.mount("#app")
+// createApp(App).use(store).use(router).mount('#app')
