6b64308b0db96b1c94de84d6e8fb7a41ce6986ef
whytomato <1158152324@qq.com>
2023-11-28 20:59:06 +0800

    1

---
+++ src/views/user/UserProfile.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                user:this.$Cookies.get('user_info')
+            }
+        }
+        
+    }
+</script>
+
+<style  scoped>
+
+</style>
