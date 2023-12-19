d9b37d984ac6b6a9276018eab3d04a2916e36078
Chisato <2514962096@qq.com>
2023-12-19 21:58:09 +0800

    version 1

---
+++ src/http/request.js
@@ -0,0 +1,38 @@
+// request.js
+import axios from 'axios';
+import {useGlobalStore} from "@/stores/global";
+const globalStore = useGlobalStore();
+const service = axios.create({
+    baseURL: "http://123.249.124.181/api",
+    // 不进行超时中断
+    timeout: 0,
+})
+// 请求拦截器
+service.interceptors.request.use(
+    (config)=>{
+        const token = globalStore.token;
+        if (token && token .length>0) {
+            // 问了下fgg,不需要Bearer
+            config.headers.Authorization = token;
+        }
+        return config;
+    },
+    (error)=>{
+        // 发生请求错误
+        return Promise.reject(error);
+    }
+)
+// 响应拦截器
+service.interceptors.response.use(
+    function(response){
+        // 2xx 都会触发该函数
+        // 如果不喜欢可以在开发过程中注释掉这行
+        // console.log("🚀response data🚀",response.data);
+        return response;
+    },
+    function (error){
+        // 发生响应错误
+        return Promise.reject(error);
+    }
+)
+export default service;
