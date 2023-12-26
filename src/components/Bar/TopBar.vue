a04c387ce7172b62056273864fb3f2f4cd8f3a3f
illuminateC <21371319@buaa.edu.cn>
2023-12-26 14:54:45 +0800

    translate && welcome

--- src/components/Bar/TopBar.vue
+++ src/components/Bar/TopBar.vue
@@ -4,7 +4,6 @@
             <div class="nav_bar_icon">
                 <div class="nav_bar_icon_title_bold" @click="backtostartpage">Spider</div>
                 <div class="nav_bar_icon_title" @click="backtostartpage">Scholar</div>
-                <el-button class="view" type="text" @click="change">{{ $t("message.language") }}</el-button>
             </div>
             <div class="nav_bar_action_frame">
                 <div v-if="logged_in === false">
